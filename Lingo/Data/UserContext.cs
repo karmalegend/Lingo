ee625b41a079f4688d55c03f5ba82b08641ecb3e
Bart <bart26nl@gmail.com>
2020-11-16 20:41:35 +0100

    init

---
+++ Lingo/Data/UserContext.cs
@@ -0,0 +1,16 @@
+﻿using Lingo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lingo.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> opt) : base(opt) { }
+
+        public DbSet<userModel> userModels { get; set; }
+    }
+}
---
+++ Lingo/Models/userModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lingo.Models
+{
+    public class userModel
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
