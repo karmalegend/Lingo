24807e7edf9b00f78f408ed3d277a8c8441065f8
Bart O <bart26nl@gmail.com>
2020-12-11 16:22:44 +0100

    Gamesession and user UT

---
+++ LingoTest/ModelTests/gameSessionModelTest.cs
@@ -0,0 +1,38 @@
+﻿using Lingo.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LingoTest.ModelTests
+{
+    [TestClass]
+    public class gameSessionModelTest
+    {
+        private gameSessionModel initObject() {
+            gameSessionModel model = new gameSessionModel();
+            model.Guesses = 0;
+            model.lastGuess = DateTime.Now;
+            return model;
+        }
+
+        [TestMethod]
+        public void increaseGuessTest() {
+            gameSessionModel model = initObject();
+            model.increaseGuess();
+            Assert.AreEqual(model.Guesses, 1);
+        }
+
+        [TestMethod]
+        public void guessedIntimeTestIntime() {
+            gameSessionModel model = initObject();
+            Assert.IsTrue(model.guessedIntime(DateTime.Now.AddSeconds(5)));
+        }
+
+        [TestMethod]
+        public void guessedIntimeTestOutOfTime() {
+            gameSessionModel model = initObject();
+            Assert.IsFalse(model.guessedIntime(DateTime.Now.AddSeconds(11)));
+        }
+    }
+}
